1b491c08102385a80947b3dae5d014fac71452ea
Luis Bispo <luisguilhermebbispo@hotmail.com>
2024-01-27 16:55:09 -0300

    Strutcture Login/Pages

--- gymApp/src/app/app.routes.ts
+++ gymApp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component:  AppComponent},
+  { path: 'home', component:  HomeComponent},
+  { path: 'login', component:  LoginComponent},
+
+];
