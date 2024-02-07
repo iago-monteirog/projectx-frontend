1d4c536c9a9ee9558f806e7a17d0c8b7342f1e8c
Luis Bispo <luisguilhermebbispo@hotmail.com>
2024-02-07 20:58:46 -0300

    feat: page Home with Menu, Content and Footer

--- gymApp/src/app/pages/home/home.component.ts
+++ gymApp/src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/shared/header/header.component";
+import { ContentComponent } from "../content/content.component";
+import { FooterComponent } from "../../components/shared/footer/footer.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, ContentComponent, FooterComponent]
 })
 export class HomeComponent {
 
