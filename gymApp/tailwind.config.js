8e57192938ec1fc450ed255394e6374504c8d08f
iago-monteirog <iagomgarcia9000@gmail.com>
2024-02-09 22:48:17 -0300

    feat: add tailwindcss and apply into main page

---
+++ gymApp/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        'navy': '#4B89AC',
+        gray: {
+          50: '#eaeaea',
+          100: '#bebebf',
+          200: '#9e9ea0',
+          300: '#727275',
+          400: '#56565a',
+          500: '#2c2c31',
+          600: '#28282d',
+          700: '#1f1f23',
+          800: '#18181b',
+          900: '#121215',
+        },
+      }
+    }
+  },
+  plugins: [],
+}
+
