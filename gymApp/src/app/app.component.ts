2fad0a667a07ed9e33464a5ac97f3bed8feab946
iago-monteirog <iagomgarcia9000@gmail.com>
2024-02-18 19:45:54 -0300

    do routing and generate login and register components pages

--- gymApp/src/app/app.component.ts
+++ gymApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from './modules/pages/login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
--- gymApp/src/app/app.routes.ts
+++ gymApp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './modules/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './modules/pages/register-page/register-page.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' }
+];
