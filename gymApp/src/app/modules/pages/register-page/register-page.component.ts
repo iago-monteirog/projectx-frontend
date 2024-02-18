7862991372c40c1653654346e1cf491db90f9d5e
iago-monteirog <iagomgarcia9000@gmail.com>
2024-02-18 19:44:37 -0300

    do routing and generate login and register components pages

---
+++ gymApp/src/app/modules/pages/register-page/register-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-register-page',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.scss'
+})
+export class RegisterPageComponent {
+
+}
