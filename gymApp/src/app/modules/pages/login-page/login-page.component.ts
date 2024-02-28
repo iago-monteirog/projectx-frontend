69124cb57d0207217cf337716f4f977826f0dd2d
iago-monteirog <iagomgarcia9000@gmail.com>
2024-02-28 19:59:02 -0300

    feature: add reactive forms to login page. (WIP)

--- gymApp/src/app/modules/pages/login-page/login-page.component.ts
+++ gymApp/src/app/modules/pages/login-page/login-page.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+    loginForm = new FormGroup({
+      login: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(20)
+      ]),
+      currentPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8)
+      ])
+    })
 
+    get login() {
+      return this.loginForm.get('login');
+    }
+
+    get currentPassword() {
+      return this.loginForm.get('currentPassword');
+    }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    console.log(this.login?.errors?.['required']);
+  }
 }
